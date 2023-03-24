4c46c9839915bc09e662e3b8800f5b656bdcd9df
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-24 21:36:38 +0700

    Update buttons.dart

--- lib/components/buttons/buttons.dart
+++ lib/components/buttons/buttons.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Buttons {
+  static Widget flat({
+    String label = 'Save',
+    bool isLoading = false,
+    VoidCallback? onPressed
+  }) {
+    return FilledButton(
+      onPressed: onPressed,
+      style: const ButtonStyle(
+          padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(vertical: 15))
+      ),
+      child: isLoading ? Loadings.basic(
+          size: const Size(27, 27),
+          color: Colors.white
+      ) : Text(
+          label,
+          style: Get.textTheme.titleLarge?.copyWith(
+              fontSize: 18,
+              color: Colors.white
+          )
+      ),
+    );
+  }
+
   static Widget floatingBottomButton({
     String label = 'Save',
     bool isLoading = false,
@@ -15,22 +38,7 @@ class Buttons {
         child: Container(
           padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
           color: Colors.white,
-          child: FilledButton(
-            onPressed: onPressed,
-            style: const ButtonStyle(
-                padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(vertical: 15))
-            ),
-            child: isLoading ? Loadings.basic(
-                size: const Size(23, 23),
-                color: Colors.white
-            ) : Text(
-                label,
-                style: Get.textTheme.titleLarge?.copyWith(
-                    fontSize: 18,
-                    color: Colors.white
-                )
-            ),
-          ),
+          child: flat(label: label, isLoading: isLoading, onPressed: onPressed),
         )
     );
   }
